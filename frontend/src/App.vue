a8dfd9df2c85df9278eac8d63618c563beeb6bcc
Christian Emmanuel Zambrano Hernandez <christianzambher@gmail.com>
2026-05-12 08:13:20 -0600

    style: Change Favicon and Title in FrontEnd

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,5 @@
 <script setup>
-  // import { onMounted, ref } from 'vue';
-  // import axios from 'axios';
   import XmlUploader from './components/XmlUploader.vue'
-
-  // const apiMessage = ref('Loading...');
-
-  // onMounted(async () => {
-  //   try {
-  //     const response = await axios.get('http://localhost:8080/api/health');
-  //     apiMessage.value = response.data.message;
-  //   } catch (error) {
-  //     apiMessage.value = 'API Connection Failed';
-  //     console.error(error);
-  //   }
-  // });
 </script>
 
 <template>
