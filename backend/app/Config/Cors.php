269fc13f3e7fca0b000fbe7975c1d41e4a956dd0
Christian Emmanuel Zambrano Hernandez <christianzambher@gmail.com>
2026-05-13 08:40:26 -0600

    feat: configure cors for local and vercel environments

--- backend/app/Config/Cors.php
+++ backend/app/Config/Cors.php
@@ -34,7 +34,7 @@ class Cors extends BaseConfig
          *   - ['http://localhost:8080']
          *   - ['https://www.example.com']
          */
-        'allowedOrigins' => ['http://localhost:5173'],
+        'allowedOrigins' => ['http://localhost:5173',],
 
         /**
          * Origin regex patterns for the `Access-Control-Allow-Origin` header.
@@ -47,7 +47,7 @@ class Cors extends BaseConfig
          * E.g.:
          *   - ['https://\w+\.example\.com']
          */
-        'allowedOriginsPatterns' => [],
+        'allowedOriginsPatterns' => [ 'https://.*\.vercel\.app',],
 
         /**
          * Weather to send the `Access-Control-Allow-Credentials` header.
@@ -68,7 +68,11 @@ class Cors extends BaseConfig
          *
          * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
          */
-        'allowedHeaders' => [],
+        'allowedHeaders' => [
+            'Content-Type',
+            'Authorization',
+            'X-Requested-With',
+        ],
 
         /**
          * Set headers to expose.
@@ -93,7 +97,11 @@ class Cors extends BaseConfig
          *
          * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
          */
-        'allowedMethods' => [],
+        'allowedMethods' => [
+            'GET',
+            'POST',
+            'OPTIONS',
+        ],
 
         /**
          * Set how many seconds the results of a preflight request can be cached.
